455f4ab46499471ebea19b82ce24ab725b02c244
pacable <77161122+pxc1984@users.noreply.github.com>
2024-08-02 20:45:03 +0300

    сильно дебафнул метеоры. Этот билд не про них (#4)

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -128,13 +128,13 @@ namespace Content.Shared.CCVar
         ///     Minimum time between meteor swarms in minutes.
         /// </summary>
         public static readonly CVarDef<float>
-            MeteorSwarmMinTime = CVarDef.Create("events.meteor_swarm_min_time", 12.5f, CVar.ARCHIVE | CVar.SERVERONLY);
+            MeteorSwarmMinTime = CVarDef.Create("events.meteor_swarm_min_time", 5.0f, CVar.ARCHIVE | CVar.SERVERONLY);  // Sunrise-Scp edit
 
         /// <summary>
         ///     Maximum time between meteor swarms in minutes.
         /// </summary>
         public static readonly CVarDef<float>
-            MeteorSwarmMaxTime = CVarDef.Create("events.meteor_swarm_max_time", 17.5f, CVar.ARCHIVE | CVar.SERVERONLY);
+            MeteorSwarmMaxTime = CVarDef.Create("events.meteor_swarm_max_time", 30.0f, CVar.ARCHIVE | CVar.SERVERONLY);  // Sunrise-Scp edit
 
         /*
          * Game
