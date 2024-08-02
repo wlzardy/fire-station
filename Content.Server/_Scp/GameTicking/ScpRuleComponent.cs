c6f21f742a35da29dcad283ea4147eb0004a0584
pacable <77161122+pxc1984@users.noreply.github.com>
2024-08-02 20:45:44 +0300

    Карта СЦП на планете (#2)
    * оно заработало, победа
    
    * долой грязные станции
    
    * пинпоинтер на станцию
    
    * генератор имен зоны
    
    * добавлен зачаток геймрула сцп для всех дальнейших разработок связанных с ним

---
+++ Content.Server/_Scp/GameTicking/ScpRuleComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Content.Server._Scp.GameTicking;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class ScpRuleComponent : Component
+{
+    
+}
---
+++ Content.Server/_Scp/NameGenerators/ScpSiteNameGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Content.Server.Maps.NameGenerators;
+using JetBrains.Annotations;
+using Robust.Shared.Random;
+
+namespace Content.Server._Scp.NameGenerators;
+
+[UsedImplicitly]
+public sealed partial class ScpSiteNameGenerator : StationNameGenerator
+{
+    [DataField("prefixCreator")] public string PrefixCreator = default!;
+
+    public override string FormatName(string input)
+    {
+        var random = IoCManager.Resolve<IRobustRandom>();
+
+        return string.Format(input, $"{random.Next(1, 83):D2}");
+    }
+}
