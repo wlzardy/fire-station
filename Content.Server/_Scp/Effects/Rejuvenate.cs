a0a36c59b9bb7838fac278f90f7ed3db8d0bbddd
pacable <77161122+pxc1984@users.noreply.github.com>
2024-08-20 23:09:06 +0300

    scp500 (#29)

---
+++ Content.Server/_Scp/Effects/Rejuvenate.cs
@@ -0,0 +1,21 @@
+﻿using Content.Server.Administration.Systems;
+using Content.Shared.EntityEffects;
+using Robust.Shared.Prototypes;
+using Serilog;
+
+namespace Content.Server._Scp.Effects;
+
+public sealed partial class Rejuvenate : EntityEffect
+{
+    protected override string? ReagentEffectGuidebookText(IPrototypeManager prototypeManager,
+        IEntitySystemManager entitySystemManager)
+    {
+        return Loc.GetString("reagent-effect-guidebook-scp500");
+    }
+
+    public override void Effect(EntityEffectBaseArgs args)
+    {
+        Log.Debug($"rejuvenated {args.TargetEntity}");
+        args.EntityManager.System<RejuvenateSystem>().PerformRejuvenate(args.TargetEntity);
+    }
+}
