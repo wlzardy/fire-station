88a0c147a27a1f771d625a04772c22b4e9f51ac3
rhailrake <splatt.pr@gmail.com>
2024-08-30 19:13:43 +0500

    gen template

---
+++ Content.Server/Procedural/DungeonJob/DungeonJob.PostLimitedEntity..cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Content.Shared.Procedural;
+using Content.Shared.Procedural.PostGeneration;
+using Robust.Shared.Random;
+
+namespace Content.Server.Procedural.DungeonJob;
+
+public sealed partial class DungeonJob
+{
+    /// <summary>
+    /// Generates limited entities in the dungeon based on the specified parameters.
+    /// <see cref="LimitedEntityDunGen"/>
+    /// </summary>
+    private async Task PostGen(LimitedEntityDunGen gen, Dungeon dungeon, Random random)
+    {
+        if (gen.Limit <= 0 || string.IsNullOrEmpty(gen.Entity))
+            return;
+
+        var checkedTiles = new HashSet<Vector2i>();
+        var candidateTiles = new List<Vector2i>(dungeon.RoomTiles);
+        random.Shuffle(candidateTiles);
+
+        var entitiesSpawned = 0;
+        foreach (var tile in candidateTiles.TakeWhile(_ => entitiesSpawned < gen.Limit).Where(tile => checkedTiles.Add(tile) && _anchorable.TileFree(_grid, tile, DungeonSystem.CollisionLayer, DungeonSystem.CollisionMask)))
+        {
+            if (TrySpawnEntity(tile, gen.Entity))
+                entitiesSpawned++;
+
+            if (entitiesSpawned % 10 == 0)
+                await SuspendDungeon();
+        }
+    }
+
+    private bool TrySpawnEntity(Vector2i tile, string entityPrototype)
+    {
+        var gridPos = _grid.GridTileToLocal(tile);
+        var entityUid = _entManager.SpawnEntity(entityPrototype, gridPos);
+
+        return entityUid != EntityUid.Invalid;
+    }
+}
