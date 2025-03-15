6d141d9a14622085abfc3954347b0450c6e02ade
ThereDrD <88589686+ThereDrD0@users.noreply.github.com>
2025-03-15 20:38:47 +0300

    Эхо (#245)

---
+++ Content.Shared/_Scp/Audio/AudioEffectsManagerSystem.cs
@@ -0,0 +1,84 @@
+﻿using Robust.Shared.Audio;
+using Robust.Shared.Audio.Components;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Network;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
+
+namespace Content.Shared._Scp.Audio;
+
+public sealed class AudioEffectsManagerSystem : EntitySystem
+{
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly IPrototypeManager _prototype = default!;
+    [Dependency] private readonly INetManager _net = default!;
+
+    /// <summary>
+    /// Захешированные эффекты под их прототипами пренитов. Позволяет не засрать слоты OpenAL сотней одинаковых эффектов
+    /// </summary>
+    private static readonly Dictionary<ProtoId<AudioPresetPrototype>, EntityUid> CachedEffects = new ();
+
+    private static readonly TimeSpan RaceConditionWaiting = TimeSpan.FromTicks(10L);
+
+    /// <summary>
+    /// Добавляет переданный эффект к звуку
+    /// </summary>
+    public bool TryAddEffect(Entity<AudioComponent> sound, ProtoId<AudioPresetPrototype> preset)
+    {
+        if (!CachedEffects.TryGetValue(preset, out var effect) && !TryCreateEffect(preset, out effect))
+            return false;
+
+        // ЕБАННЫЙ РОТ ЭТОГО РЕЙС КОДИШЕН
+        /*
+         Лонг-рид причина почему тут стоит таймер:
+         Как только only server-side звук приходит сюда, он вызывает только серверную систему добавления Auxiliary
+         Тот вызывает Dirty(), который отлавливается на клиенте вручную через компонент стейт
+         Там на аудио сурс навешивается эффект. Только по умолчанию сурс это дамми(заглушка)
+         Аудио сурс выставляется на подписке AudioComponent на ComponentStartup().
+         Так как я могу подписаться только ComponentInit(), который идет раньше, чем ComponentStartup()
+         То мой ивент происходит раньше, чем выставляется аудиосурс на клиенте -> сервер успевает втиснуться в промежуток между этой хуйней
+         И добавить эффект на заглушку, которая ниче не сделает. Поэтому я на рандом поставил сюда 10 тиков, за это время все успевает сработать
+         ГОВНО
+         */
+        if (_net.IsServer)
+        {
+            Timer.Spawn(RaceConditionWaiting, () => _audio.SetAuxiliary(sound, sound, effect));
+        }
+        else
+        {
+            _audio.SetAuxiliary(sound, sound, effect);
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Пытается создать эффект и захешировать его
+    /// </summary>
+    /// <param name="preset">Пресет эффектов</param>
+    /// <param name="effectStuff">Получаемый эффект. Не представляет собой ничего, когда метод возвращает false</param>
+    /// <returns>Возвращает успешно ли создание и хеширование эффекта</returns>
+    public bool TryCreateEffect(ProtoId<AudioPresetPrototype> preset, out EntityUid effectStuff)
+    {
+        effectStuff = default;
+
+        if (!_prototype.TryIndex(preset, out var prototype))
+            return false;
+
+        var effect = _audio.CreateEffect();
+        var auxiliary = _audio.CreateAuxiliary();
+
+        _audio.SetEffectPreset(effect.Entity, effect.Component, prototype);
+        _audio.SetEffect(auxiliary.Entity, auxiliary.Component, effect.Entity);
+
+        if (!Exists(auxiliary.Entity))
+            return false;
+
+        if (!CachedEffects.TryAdd(preset, auxiliary.Entity))
+            return false;
+
+        effectStuff = auxiliary.Entity;
+
+        return true;
+    }
+}
---
+++ Content.Shared/_Scp/Audio/Systems/EchoEffectSystem.cs
@@ -0,0 +1,42 @@
+﻿using Robust.Shared.Audio;
+using Robust.Shared.Audio.Components;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
+
+namespace Content.Shared._Scp.Audio.Systems;
+
+public sealed class EchoEffectSystem : EntitySystem
+{
+    [Dependency] private readonly AudioEffectsManagerSystem _effectsManager = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+
+    private static readonly ProtoId<AudioPresetPrototype> EchoEffectPreset = "SewerPipe";
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<AudioComponent, ComponentInit>(OnInit, before: [typeof(SharedAudioSystem)]);
+    }
+
+    private void OnInit(Entity<AudioComponent> ent, ref ComponentInit args)
+    {
+        ApplyEcho(ent);
+    }
+
+    public void ApplyEcho(Entity<AudioComponent> sound, ProtoId<AudioPresetPrototype>? preset = null)
+    {
+        if (!_timing.IsFirstTimePredicted)
+            return;
+
+        if (!Exists(sound))
+            return;
+
+        // Фоновая музыка не должна подвергаться эффектам эха
+        if (sound.Comp.Global)
+            return;
+
+        _effectsManager.TryAddEffect(sound, preset ?? EchoEffectPreset);
+    }
+}
