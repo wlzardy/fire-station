c100fd06f315f7b358758cfc3b8abb7be706d163
ThereDrD <88589686+ThereDrD0@users.noreply.github.com>
2025-03-17 20:13:01 +0300

    Фиксы аудио-эффектов (#249)

--- Content.Shared/_Scp/Audio/AudioEffectsManagerSystem.cs
+++ Content.Shared/_Scp/Audio/AudioEffectsManagerSystem.cs
@@ -1,9 +1,11 @@
-﻿using Robust.Shared.Audio;
+﻿using System.Threading;
+using Content.Shared.GameTicking;
+using Robust.Shared.Audio;
 using Robust.Shared.Audio.Components;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Network;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Timing;
+using Timer = Robust.Shared.Timing.Timer;
 
 namespace Content.Shared._Scp.Audio;
 
@@ -17,8 +19,24 @@ public sealed class AudioEffectsManagerSystem : EntitySystem
     /// Захешированные эффекты под их прототипами пренитов. Позволяет не засрать слоты OpenAL сотней одинаковых эффектов
     /// </summary>
     private static readonly Dictionary<ProtoId<AudioPresetPrototype>, EntityUid> CachedEffects = new ();
+    private static CancellationTokenSource _tokenSource = new();
 
     private static readonly TimeSpan RaceConditionWaiting = TimeSpan.FromTicks(10L);
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<RoundRestartCleanupEvent>(_ => Clear());
+    }
+
+    private static void Clear()
+    {
+        CachedEffects.Clear();
+
+        _tokenSource.Cancel();
+        _tokenSource = new();
+    }
 
     /// <summary>
     /// Добавляет переданный эффект к звуку
@@ -42,7 +60,7 @@ public sealed class AudioEffectsManagerSystem : EntitySystem
          */
         if (_net.IsServer)
         {
-            Timer.Spawn(RaceConditionWaiting, () => _audio.SetAuxiliary(sound, sound, effect));
+            Timer.Spawn(RaceConditionWaiting, () => _audio.SetAuxiliary(sound, sound, effect), _tokenSource.Token);
         }
         else
         {
