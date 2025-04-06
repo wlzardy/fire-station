932581e9ebf01250294ee7ec4cbdbab53cd8af98
Vigers Ray <vigersray@gmail.com>
2025-04-06 06:58:42 +0300

    фиксы апстрима

--- Content.Server/_Scp/Body/Systems/SmokeFilterSystem.cs
+++ Content.Server/_Scp/Body/Systems/SmokeFilterSystem.cs
@@ -30,7 +30,7 @@ public sealed class SmokeFilterSystem : EntitySystem
 
     private void OnMaskToggled(Entity<SmokeFilterComponent> ent, ref ItemMaskToggledEvent args)
     {
-        if (args.IsToggled || args.IsEquip)
+        if (args.Mask.Comp.IsToggled || args.Wearer != null)
         {
             ent.Comp.IsActive = false;
         }
