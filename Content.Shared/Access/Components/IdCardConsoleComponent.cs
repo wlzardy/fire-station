8a6514653a53d72c017932f3b8c4a10838003c39
ThereDrD <88589686+ThereDrD0@users.noreply.github.com>
2025-04-09 12:43:32 +0300

    Фикс обрезанных должностей в айди картах (#279)

--- Content.Shared/Access/Components/IdCardConsoleComponent.cs
+++ Content.Shared/Access/Components/IdCardConsoleComponent.cs
@@ -10,8 +10,8 @@ namespace Content.Shared.Access.Components;
 [Access(typeof(SharedIdCardConsoleSystem))]
 public sealed partial class IdCardConsoleComponent : Component
 {
-    public const int MaxFullNameLength = 30;
-    public const int MaxJobTitleLength = 30;
+    public const int MaxFullNameLength = 90; // Fire edit
+    public const int MaxJobTitleLength = 90; // Fire edit
 
     public static string PrivilegedIdCardSlotId = "IdCardConsole-privilegedId";
     public static string TargetIdCardSlotId = "IdCardConsole-targetId";
